ef8906033fc3da503eef25189ffbb0f69da3dc0e
kaushik <vezzu.hiteshsrisaikaushik@Lumen.com>
2024-01-10 12:54:42 +0530

    api added

---
+++ src/main/java/com/example/demo/api/sampleapi.java
@@ -0,0 +1,17 @@
+package com.example.demo.api;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("kaushikurl")
+public class sampleapi
+{
+    @GetMapping
+    public ResponseEntity<String> get()
+    {
+        return ResponseEntity.ok("hello my name is kaushik");
+    }
+}
